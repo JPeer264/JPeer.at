cae313381dfd1674723a9f5ee4e6322364a9287c
JPeer264 <jan.oster94@gmail.com>
2016-07-31 14:49:23 +0200

    updated timeline using the reveal service

--- src/cmps/timeline/timeline.directive.js
+++ src/cmps/timeline/timeline.directive.js
@@ -9,7 +9,11 @@ angular
  * @description
  * Generates a single timeline component.
  */
-function timelineDirective() {
+timelineDirective.$inject = [
+    'RevealService'
+];
+
+function timelineDirective(RevealService) {
         // Runs during compile
         return {
             // name: '',
@@ -25,7 +29,54 @@ function timelineDirective() {
             replace: true,
             // transclude: true,
             // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-            link: function($scope, iElm, iAttrs, controller) {
+            link: function(scope, iElm, iAttrs, controller) {
+                // class instances
+                var boxReveal = new RevealService();
+                var bgReveal = new RevealService();
+                var $$ = new JP.SelectorCache();
+
+                var $boxes = $$.get('.jp-timeline__box');
+                var $window = $$.get(window);
+                var $circles = $$.get('.jp-timeline--circle');
+                var $background = $$.get('.jp-timeline__background');
+                var windowHeight = $window.height();
+                var triggerHeight = windowHeight * 0.6; // from bottom of the screen
+                var bgClassName = 'jp-timeline__background--green';
+                var scroll;
+
+                $circles.each(function (key, value) {
+                    var $this = $(this);
+
+                    // reveal boxes
+                    boxReveal.reveal({
+                        triggerHeight: triggerHeight
+                    }, $this, function ($trigger) {
+                        $($boxes[key]).removeClass('jp-timeline__box--hidden');
+                        $($boxes[key]).css({
+                            top: $trigger.position().top,
+                        });
+                    }, function () {
+                        $($boxes[key]).addClass('jp-timeline__box--hidden');
+                    });
+
+                    bgReveal.custom({
+                        triggerHeight: triggerHeight,
+                        delayedTriggerHeight: 340
+                    }, $this, function ($trigger, that) {
+                        var thisOffsetTop = $trigger.offset().top;
+
+                        // check height and add new color
+                        if ((thisOffsetTop - triggerHeight) < that.scroll && (thisOffsetTop - triggerHeight) + 150 > that.scroll) {
+                            $background.addClass(bgClassName);
+                        } else {
+
+                            // remove just after one element - but also before the first
+                            if (thisOffsetTop - triggerHeight < that.scroll || (key === 0 && thisOffsetTop - triggerHeight > that.scroll )) {
+                                $background.removeClass(bgClassName);
+                            }
+                        }
+                    });
+                });
             }
         };
 };
