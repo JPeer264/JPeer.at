4cded62073059847a0600790d67eb4b36c10e79f
JPeer264 <jan.oster94@gmail.com>
2017-05-28 11:07:48 +0200

    Feat: add service worker

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,21 @@
-console.log('test');
+self.addEventListener('fetch', function fetcher(event) {
+  const request = event.request;
+
+  if (request.url.indexOf('assets') > -1) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+      caches.match(event.request).then((response) => (
+        // response if there is a cache
+        // fetch if there is nothing cached and then cache it
+        response || fetch(request).then(response_two => {
+          caches
+            .open('JPEER_CACHE_SW')
+            .then((cache) => {
+              cache.put(request.url, response_two);
+            });
+        })
+      ))
+    );
+  }
+});
