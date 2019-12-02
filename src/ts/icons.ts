19e941e87116712d19c734770580bc3a5f31dfa7
JPeer264 <jan.oster94@gmail.com>
2019-12-02 10:12:59 +0100

    Feat: add email

--- src/ts/icons.ts
+++ src/ts/icons.ts
@@ -1,7 +1,9 @@
 import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 
-library.add(faGithub, faLinkedin);
+library.add(faGithub, faLinkedin, faEnvelope);
 
 config.autoReplaceSvg = 'nest';
 dom.watch();
