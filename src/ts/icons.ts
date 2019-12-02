9c60acdb0cac9c27a17dd34766f89ba7821f2b10
JPeer264 <jan.oster94@gmail.com>
2019-12-02 10:34:28 +0100

    Feat: add stackshare

--- src/ts/icons.ts
+++ src/ts/icons.ts
@@ -2,8 +2,14 @@ import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons/faShareAlt';
 
-library.add(faGithub, faLinkedin, faEnvelope);
+library.add(
+  faGithub,
+  faLinkedin,
+  faEnvelope,
+  faShareAlt,
+);
 
 config.autoReplaceSvg = 'nest';
 dom.watch();
